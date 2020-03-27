5780bd341e13fa7bd9c14cc73ceee4725965aa54
Kitrick Bell <kitrickd.bell@gmail.com>
2020-03-27 16:31:55 -0500

    Deploy

--- client/src/pages/settings.js
+++ client/src/pages/settings.js
@@ -1,108 +1,106 @@
-import React, { useState, useEffect } from "react";
-import to from 'await-to-js';
-import "./styles/settings/style.css";
+// import React, { useState, useEffect } from "react";
+// import to from 'await-to-js';
+// import "./styles/settings/style.css";
 
-import Notifications from "../components/SettingsGroup/notifications.js";
-import Securiprivacy from "../components/SettingsGroup/securiprivacy.js";
-import Geolocation from "../components/SettingsGroup/geolocation.js";
-import Account from "../components/SettingsGroup/account.js";
-import Toggle from "../components/SettingsGroup/toggle.js";
-import API from "../utils/API";
-import GEOAPI from "../utils/geolocation/API.js";
-import KeyContainer from "../components/SettingsGroup/streamingkey";
-import streamGuide from "../components/SettingsGroup/streamGuide";
+// import Notifications from "../components/SettingsGroup/notifications.js";
+// import Securiprivacy from "../components/SettingsGroup/securiprivacy.js";
+// import Geolocation from "../components/SettingsGroup/geolocation.js";
+// import Account from "../components/SettingsGroup/account.js";
+// import Toggle from "../components/SettingsGroup/toggle.js";
+// import API from "../utils/API";
+// import GEOAPI from "../utils/geolocation/API.js";
+// import KeyContainer from "../components/SettingsGroup/streamingkey";
+// import streamGuide from "../components/SettingsGroup/streamGuide";
 
-// TODO:This is not a real import. This helps me follow a train of thought. REMOVE AFTER DONE.
-import MongooseData from "mongoose";
+// // TODO:This is not a real import. This helps me follow a train of thought. REMOVE AFTER DONE.
+// import MongooseData from "mongoose";
 
 
-const Settings = (props) => {
+// const Settings = (props) => {
     
-    const [component, setComponent] = useState("Geolocation");
-    const [results, setResults] = useState(['No Location on file!']);
-    const [key, setKey] = useState({streamKey: ''});
+//     const [component, setComponent] = useState("Geolocation");
+//     const [results, setResults] = useState(['No Location on file!']);
+//     const [key, setKey] = useState({streamKey: ''});
 
-    // TODO:REPLACE WITH MONGOOSE SEEDS
-    const exampleChefs = ["Murphy","21 Savage", "23 Savage", "Joe Mama"];
-    let local;
+//     // TODO:REPLACE WITH MONGOOSE SEEDS
+//     const exampleChefs = ["Murphy","21 Savage", "23 Savage", "Joe Mama"];
+//     let local;
 
-    useEffect(() => { 
-        loadStreamKey();
-    });
+//     useEffect(() => { 
+//         loadStreamKey();
+//     });
 
-    const loadStreamKey = async (props) => {
-        const res = await API.getStreamKey();
-        setKey({streamKey: res.data.streamKey})
-    }
+//     const loadStreamKey = async (props) => {
+//         const res = await API.getStreamKey();
+//         setKey({streamKey: res.data.streamKey})
+//     }
     
-    const postStreamKey = async (props) => {
-        const res = await API.postStreamKey();
-        setKey({streamKey: res.data.streamKey})
-    }
-    const locationHandler = async () => {
-        const [dataError, data] = await to(GEOAPI.Locate());
-        if(dataError) return;
+//     const postStreamKey = async (props) => {
+//         const res = await API.postStreamKey();
+//         setKey({streamKey: res.data.streamKey})
+//     }
+//     const locationHandler = async () => {
+//         const [dataError, data] = await to(GEOAPI.Locate());
+//         if(dataError) return;
 
-        const location = data.data.results[4].formatted_address;
-        setResults(location);
-    };
+//         const location = data.data.results[4].formatted_address;
+//         setResults(location);
+//     };
 
-    const componentMap = {
-        Notifications:
-        <>
-            <h1>Notifications</h1>
-            <Notifications
-                description1 = {"Notify Via Email"}
+//     const componentMap = {
+//         Notifications:
+//         <>
+//             <h1>Notifications</h1>
+//             <Notifications
+//                 description1 = {"Notify Via Email"}
                 
-            />
-            <br></br>
-            <Toggle
-                name = {" All "}
-            />
-            {exampleChefs.map((result, i) =>(
+//             />
+//             <br></br>
+//             <Toggle
+//                 name = {" All "}
+//             />
+//             {exampleChefs.map((result, i) =>(
     
-                <Toggle
-                    key = {i}
-                    name = {result} 
-                /> 
-            ))}
-            <br></br>
-            <br></br>
-            <Notifications
-                description1 = {"Notify Via SMS Text Message"}
-            />
-            <br></br>
-            <Toggle
-                name = {" All "}
-            />
-            {exampleChefs.map((result, i) =>(
+//                 <Toggle
+//                     key = {i}
+//                     name = {result} 
+//                 /> 
+//             ))}
+//             <br></br>
+//             <br></br>
+//             <Notifications
+//                 description1 = {"Notify Via SMS Text Message"}
+//             />
+//             <br></br>
+//             <Toggle
+//                 name = {" All "}
+//             />
+//             {exampleChefs.map((result, i) =>(
                 
-                <Toggle
-                    name = {result}
-                />
-            ))}
-        </>
+//                 <Toggle
+//                     name = {result}
+//                 />
+//             ))}
+//         </>
         
         
-        ,
-        Securiprivacy: <Securiprivacy/>,
-        Geolocation: <Geolocation
-            clicked = {locationHandler}
+//         ,
+//         Securiprivacy: <Securiprivacy/>,
+//         Geolocation: <Geolocation
+//             clicked = {locationHandler}
 
 
-            location = {results}
+//             location = {results}
             
         
-        />,
-        Account: <Account/>,
-        streamingkey: <KeyContainer
-            onClick = {postStreamKey}
-        />,
-        streamGuide: <streamGuide/>
+//         />,
+//         Account: <Account/>,
+//         streamingkey: <KeyContainer
+//             onClick = {postStreamKey}
+//         />,
+//         streamGuide: <streamGuide/>
 
-}
-
-
+// }
 
 
 
@@ -111,43 +109,45 @@ const Settings = (props) => {
 
 
 
-    return (
-        <React.Fragment>
-            <aside>
-                <h1>Settings</h1>
-                <ul>
-                    <li tabIndex="1" className = "nav-item active border-bottom">
-                        <a className = "nav-link" type = "button" onClick={() => setComponent("Notifications")}>Notifications</a>
-                    </li>
-                    <li  tabIndex="1"className = "nav-item border-bottom" >
-                        <a className = "nav-link" type = "button" onClick = {() => setComponent("Geolocation")}>Geolocation</a>
-                    </li>
-                    <li  tabIndex="1"className = "nav-item border-bottom" >
-                        <a className = "nav-link" type = "button" onClick = {() => setComponent("Securiprivacy")}>Security & Privacy</a>
-                    </li>
-                    <li  tabIndex="1" className = "nav-item border-bottom" >
-                        <a className = "nav-link" type = "button" onClick = {() => setComponent("Account")}>Account</a>
-                    </li>
-                    <li  tabIndex="1" className = "nav-item border-bottom" >
-                        <a className = "nav-link" type = "button" onClick = {() => setComponent("streamingkey")}>StreamingKey</a>
-                    </li>
-                    <li  tabIndex="1" className = "nav-item border-bottom" >
-                        <a className = "nav-link" type = "button" onClick = {() => setComponent("streamGuide")}>Stream Guide</a>
-                    </li>
-                </ul>
-            </aside>
-            <header></header>
-            <section>
-            {componentMap[component]}
 
-            </section>
-            <section></section>
+
+//     return (
+//         <React.Fragment>
+//             <aside>
+//                 <h1>Settings</h1>
+//                 <ul>
+//                     <li tabIndex="1" className = "nav-item active border-bottom">
+//                         <a className = "nav-link" type = "button" onClick={() => setComponent("Notifications")}>Notifications</a>
+//                     </li>
+//                     <li  tabIndex="1"className = "nav-item border-bottom" >
+//                         <a className = "nav-link" type = "button" onClick = {() => setComponent("Geolocation")}>Geolocation</a>
+//                     </li>
+//                     <li  tabIndex="1"className = "nav-item border-bottom" >
+//                         <a className = "nav-link" type = "button" onClick = {() => setComponent("Securiprivacy")}>Security & Privacy</a>
+//                     </li>
+//                     <li  tabIndex="1" className = "nav-item border-bottom" >
+//                         <a className = "nav-link" type = "button" onClick = {() => setComponent("Account")}>Account</a>
+//                     </li>
+//                     <li  tabIndex="1" className = "nav-item border-bottom" >
+//                         <a className = "nav-link" type = "button" onClick = {() => setComponent("streamingkey")}>StreamingKey</a>
+//                     </li>
+//                     <li  tabIndex="1" className = "nav-item border-bottom" >
+//                         <a className = "nav-link" type = "button" onClick = {() => setComponent("streamGuide")}>Stream Guide</a>
+//                     </li>
+//                 </ul>
+//             </aside>
+//             <header></header>
+//             <section>
+//             {componentMap[component]}
+
+//             </section>
+//             <section></section>
             
-            <main></main>
+//             <main></main>
             
-        </React.Fragment>
-    )
-}
+//         </React.Fragment>
+//     )
+// }
 
 
-export default Settings;
+// export default Settings;
