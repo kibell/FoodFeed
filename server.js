be54da2024e462f0f732c2992c3e6a63f88ca5f6
Kitrick Bell <kitrickd.bell@gmail.com>
2020-03-27 16:11:06 -0500

    Deploy

--- server.js
+++ server.js
@@ -71,10 +71,10 @@ app.use(
 
 app.use(bodyParser.json());
 
-// const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/foodfeed";
-// // Connect to Mongo DB
-// mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }, err => { if(err) { console.log(err); }}).
-// then(() => console.log("MONGO DATABASE CONNECTED"));
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/foodfeed";
+// Connect to Mongo DB
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }, err => { if(err) { console.log(err); }}).
+then(() => console.log("MONGO DATABASE CONNECTED"));
 
 
 
